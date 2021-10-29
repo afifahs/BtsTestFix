e1b8a477e0b0e56298f0d979e72c667486c1eec2
Afifah <afifah_s.p@bi.go.id>
2021-10-29 10:43:43 +0700

    Add project files.

---
+++ BtsTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BtsTest.Startup))]
+namespace BtsTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
